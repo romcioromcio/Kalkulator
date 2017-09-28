5f467160129320afa9e3cb6913b6e6a8780abadf
“nasz_login_do_github <nasz_email_do_github>
2017-09-28 14:38:46 +0200

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class Calculator {
+    public int addition (int a, int b) {
+        System.out.println("It works! result = " + (a+b));
+        return a + b;
+    }
+    public int subtraction (int a, int b) {
+        System.out.println("It works! result = " + (a-b));
+        return a - b;
+    }
+}
